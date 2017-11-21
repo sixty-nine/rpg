c728e53d54ee1034603814b308139fccbbae4208
Daniel Barsotti <email>
2017-11-21 07:37:29 +0100

    Cleanup app file

--- app.py
+++ app.py
@@ -11,14 +11,19 @@ from game.markov import NameGenerator, males, females
 FLAGS = None
 
 def main(_):
-	print '-' * 80
-	for weapon in Combat.weapons:
-		print str(Combat.weapons[weapon])
-	print '-' * 80
 
-	for name, monster in Monsters.monsters.iteritems():
-		monster.dump()
+	if FLAGS.dump_weapons:
 		print '-' * 80
+		for weapon in Combat.weapons:
+			print str(Combat.weapons[weapon])
+		print '-' * 80
+		return
+
+	if FLAGS.dump_monsters:
+		for name, monster in Monsters.monsters.iteritems():
+			monster.dump()
+			print '-' * 80
+		return
 
 	m = Monsters.monsters['Goblin']
 
@@ -29,20 +34,19 @@ def main(_):
 	p.equip(Equipment.LEFT_HAND, Combat.weapons['Glaive'])
 	p.dump()
 
-	#p.attack(m, p.equipment[Equipment.RIGHT_HAND])
-	#p.attack(m, p.equipment[Equipment.LEFT_HAND])
-	#m.attack(p)
-
-	c = Combat.Combat(p, m)
-	while not p.isDead and not m.isDead:
-		c.doRound()
-	print p.name, p.hp
-	print m.name, m.hp
+	if FLAGS.combat:
+		c = Combat.Combat(p, m)
+		while not p.isDead and not m.isDead:
+			c.doRound()
+		print p.name, p.hp
+		print m.name, m.hp
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  #parser.add_argument('--set', type=str, default='train', help='The data set to use (train, test, validation)')
-  #parser.add_argument('--max', type=int, default=1, help='How many images to show')
-  FLAGS, unparsed = parser.parse_known_args()
-  main([sys.argv[0]] + unparsed)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--dump-weapons', action='store_true', help='Dump the weapons')
+	parser.add_argument('--dump-monsters', action='store_true', help='Dump the monsters')
+	parser.add_argument('--combat', action='store_true', help='Engage a fight')
+	#parser.add_argument('--max', type=int, default=1, help='How many images to show')
+	FLAGS, unparsed = parser.parse_known_args()
+	main([sys.argv[0]] + unparsed)
