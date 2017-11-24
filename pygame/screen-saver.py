4039f6e2b9d9aa834e7196b09a57357742012077
Daniel Barsotti <sixtynine.db@gmail.com>
2017-11-24 23:20:43 +0100

    rgb to hex, and vice-versa

--- pygame/screen-saver.py
+++ pygame/screen-saver.py
@@ -7,6 +7,16 @@ import sys
 
 FLAGS = None
 
+def hex_to_rgb(value):
+    """Return (red, green, blue) for the color given as #rrggbb."""
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def rgb_to_hex(red, green, blue):
+    """Return color as #rrggbb for the given color values."""
+    return '#%02x%02x%02x' % (red, green, blue)
+
 def getRandomPointInCircle(radius):
     t = 2 * math.pi * random.uniform(0, 1)
     u = random.uniform(0, 1) + random.uniform(0, 1)
@@ -54,7 +64,7 @@ def main(_):
                 done=True # Flag that we are done so we exit this loop
 
         # Clear the screen and set the screen background
-        screen.fill(WHITE)
+        screen.fill(hex_to_rgb('#0050FF'))
 
         counter += increment
         point = getRandomPointInCircle(counter)
